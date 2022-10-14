0651e4442032fabbc80131dd44314aa4139d8ddd
Pavel <pavel.sharagin.1994@gmail.com>
2022-10-14 11:48:19 +0500

    task64 done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string GetNaturalNumberTillOne(int n)
+{
+  if (1 < n) 
+  return n + ", " + GetNaturalNumberTillOne(n-1);
+  else
+  return 1.ToString();
+}
+int n = GetNumberFromConsole("Введите число: ");
+Console.Write(GetNaturalNumberTillOne(n));
